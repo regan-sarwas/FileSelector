35ad3b0338d1dec454e348cd24fd2a39c826d97e
Regan Sarwas (work mac) <regan_sarwas@nps.gov>
2013-11-18 14:51:17 -0900

    Added ability to load files from safari or mail (sort of)

--- FileSelector/FSAppDelegate.m
+++ FileSelector/FSAppDelegate.m
@@ -50,4 +50,10 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    NSLog(@"%@ asked me to open %@", sourceApplication, url);
+    return YES;
+}
+
 @end
