787523ce39fdd473392299d439633bdfaa227420
Regan Sarwas (work mac) <regan_sarwas@nps.gov>
2013-12-02 08:02:32 -0900

    merged latest code into iPad version

--- FileSelector/ProtocolDetailViewController.m
+++ FileSelector/ProtocolDetailViewController.m
@@ -23,6 +23,7 @@
     //FIXME: Autolayout bug? - title height in landscape is set by title height in portrait
     self.nameLabel.text = self.protocol.version ? [NSString stringWithFormat:@"%@, v. %@", self.protocol.title, self.protocol.version] : self.protocol.title;
     self.dateLabel.text = self.protocol.dateString;
+    //FIXME: AutoLayout issue - details not taking all available space in popover
     self.descriptionLabel.text = self.protocol.isLocal ? self.protocol.details : @"Download the protocol for more details.";
 }
 
